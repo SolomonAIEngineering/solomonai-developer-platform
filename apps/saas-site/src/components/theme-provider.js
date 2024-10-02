1a775c34e17797536b76dc4b9bc1594c94fa26d5
Lens <45818877+yoanyombapro1234@users.noreply.github.com>
2024-10-02 02:54:28 -0400

    fix: wip
    Signed-off-by: Lens <45818877+yoanyombapro1234@users.noreply.github.com>

--- apps/saas-site/src/components/theme-provider.js
+++
@@ -1,8 +0,0 @@
-"use client";
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ThemeProvider = ThemeProvider;
-const next_themes_1 = require("next-themes");
-function ThemeProvider({ children, ...props }) {
-    return <next_themes_1.ThemeProvider {...props}>{children}</next_themes_1.ThemeProvider>;
-}
