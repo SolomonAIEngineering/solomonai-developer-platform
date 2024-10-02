994161b443316d21af8f23b250d02f32865bb90e
Lens <45818877+yoanyombapro1234@users.noreply.github.com>
2024-10-02 04:56:11 -0400

    fix: add mobile app (#7)
    * fix: wip
    
    Signed-off-by: Lens <45818877+yoanyombapro1234@users.noreply.github.com>
    
    * fix: wip
    
    Signed-off-by: Lens <45818877+yoanyombapro1234@users.noreply.github.com>
    
    * fix: wip
    
    Signed-off-by: Lens <45818877+yoanyombapro1234@users.noreply.github.com>
    
    * fix: wip
    
    Signed-off-by: Lens <45818877+yoanyombapro1234@users.noreply.github.com>
    
    * fix: wip
    
    Signed-off-by: Lens <45818877+yoanyombapro1234@users.noreply.github.com>
    
    * fix: wip
    
    Signed-off-by: Lens <45818877+yoanyombapro1234@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Lens <45818877+yoanyombapro1234@users.noreply.github.com>

---
+++ apps/mobile/lint-staged.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  '**/*.{js,jsx,ts,tsx}': (filenames) => [
+    `bunx eslint --fix ${filenames
+      .map((filename) => `"${filename}"`)
+      .join(' ')} --config .eslintrc.js"`,
+  ],
+  '**/*.(md|json)': (filenames) =>
+    `bunx prettier --write ${filenames
+      .map((filename) => `"${filename}"`)
+      .join(' ')}"`,
+  'src/translations/*.(json)': (filenames) => [
+    `bunx eslint --fix ${filenames
+      .map((filename) => `"${filename}"`)
+      .join(' ')} --config .eslintrc.js"`,
+  ],
+};
