fbdf81d8f1adf6611acc06c99b5e10dd89f68e87
Lens <45818877+yoanyombapro1234@users.noreply.github.com>
2024-10-03 07:56:14 -0400

    fix: wip
    Signed-off-by: Lens <45818877+yoanyombapro1234@users.noreply.github.com>

--- apps/supabase-api/supabase/functions/send-email/index.ts
+++ apps/supabase-api/supabase/functions/send-email/index.ts
@@ -1,49 +1,63 @@
-"use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
-Object.defineProperty(exports, "__esModule", { value: true });
-const welcome_1 = require("@v1/emails/welcome");
-const react_1 = __importDefault(require("react"));
-const components_1 = require("react-email/components");
-const resend_1 = require("resend");
-const standardwebhooks_1 = require("standardwebhooks");
-const resend = new resend_1.Resend(Deno.env.get("RESEND_API_KEY"));
-const hookSecret = Deno.env.get("SEND_EMAIL_HOOK_SECRET");
+import { WelcomeEmail } from "@v1/emails/welcome";
+import React from "react";
+import { render } from "react-email/components";
+import { Resend } from "resend";
+import { Webhook } from "standardwebhooks";
+
+const resend = new Resend(Deno.env.get("RESEND_API_KEY") as string);
+const hookSecret = Deno.env.get("SEND_EMAIL_HOOK_SECRET") as string;
+
 Deno.serve(async (req) => {
   if (req.method !== "POST") {
     return new Response("not allowed", { status: 400 });
   }
+
   const payload = await req.text();
   const headers = Object.fromEntries(req.headers);
-  const wh = new standardwebhooks_1.Webhook(hookSecret);
+  const wh = new Webhook(hookSecret);
+
   const {
     user,
     email_data: { token, token_hash, redirect_to, email_action_type },
-  } = wh.verify(payload, headers);
+  } = wh.verify(payload, headers) as {
+    user: {
+      email: string;
+    };
+    email_data: {
+      token: string;
+      token_hash: string;
+      redirect_to: string;
+      email_action_type: string;
+      site_url: string;
+      token_new: string;
+      token_hash_new: string;
+    };
+  };
+
   switch (email_action_type) {
     case "signup": {
-      const html = await (0, components_1.render)(
-        react_1.default.createElement(welcome_1.WelcomeEmail),
-      );
+      const html = await render(React.createElement(WelcomeEmail));
+
       await resend.emails.send({
         from: "Create v1 <onboarding@resend.dev>",
         to: [user.email],
         subject: "Welcome to v1",
         html,
       });
+
       break;
     }
+
     // Add other email actions here
     // case 'reset_password':
     // case 'magic_link':
     default:
       throw new Error("Invalid email action type");
   }
+
   const responseHeaders = new Headers();
   responseHeaders.set("Content-Type", "application/json");
+
   return new Response(JSON.stringify({}), {
     status: 200,
     headers: responseHeaders,
